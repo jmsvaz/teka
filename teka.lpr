3db50131528557cfb0069f105e6e05c57dfd2e7d
jmvaz <jmsvaz@gmail.com>
2011-07-20 23:47:29 -0300

    added resourcestrings to prepare for internationalization

--- teka.lpr
+++ teka.lpr
@@ -7,7 +7,7 @@ uses
   cthreads,
   {$ENDIF}{$ENDIF}
   Interfaces, // this includes the LCL widgetset
-  Forms, uMain, uUtil, uColors;
+  Forms, uMain, uUtil, uColors, uStrings;
 
 {$R *.res}
 
---
+++ uStrings.pas
@@ -0,0 +1,30 @@
+unit uStrings;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+resourcestring
+
+  s_Quit = 'Quit';
+  s_Clear = 'Clear';
+  s_SpaceKey = 'SPACE';
+  s_About = 'About';
+
+  s_Circle = 'Circle';
+  s_Line   = 'Line';
+
+  s_RandomRainbowColorPallete = 'Random Rainbow Color';
+  s_RainbowColorPallete       = 'Rainbow Color';
+  s_RandomWebSafeColorPallete = 'Random WebSafe Color';
+
+  s_PathTypeNameNotFound = 'Path Type Name not found.';
+  s_ColorPalleteTypeNameNotFound = 'Color Pallete Type Name not found.';
+
+implementation
+
+end.
+
